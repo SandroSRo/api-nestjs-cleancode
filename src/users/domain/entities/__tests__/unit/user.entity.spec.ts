d40f58a359f2cb5e362529748a589191575b8996
Alessandro Rocha <alessandro.s.rocha@hotmail.com>
2023-10-05 19:24:36 -0300

    user entity tests

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker'
+import { UserEntity, UserProps } from '../../user.entity'
+
+describe('User Entity unit tests', () => {
+  let sut: UserEntity
+  let props: UserProps
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+
+    sut = new UserEntity(props)
+  })
+
+  it('Contructor method ', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+})
