88dff964e9ebc68b9e4eb9b2a5f85530e397cc36
Alessandro Rocha <alessandro.s.rocha@hotmail.com>
2023-10-05 18:55:52 -0300

    add user resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { EnvConfigModule } from './shared/infrastruture/env-config/env-config.module';
+import { UsersModule } from './users/infraestructure/users.module';
 
 @Module({
-  imports: [EnvConfigModule.forRoot()],
+  imports: [EnvConfigModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
