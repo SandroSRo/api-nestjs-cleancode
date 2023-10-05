8009a1e9bd15e30972427e6fc7b693dd8f40e8ef
Alessandro Rocha <alessandro.s.rocha@hotmail.com>
2023-10-05 14:11:20 -0300

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfigModule } from './shared/infrastruture/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [
+
+  EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
